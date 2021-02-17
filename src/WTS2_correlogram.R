29cdd39c8b50a11160adf91e60ae07d592d6b733
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-02-17 16:00:08 +0900

    ggAcf before rm comment

--- src/WTS2_correlogram.R
+++ src/WTS2_correlogram.R
@@ -1,4 +1,4 @@
-source("src/functions_WTS1.R")
+source("src/functions_WTS2.R")
 
 # # args setting
 # args <- commandArgs(trailingOnly = T)
@@ -21,16 +21,66 @@ args_celltype <-c("X1")
 
 # load
 ##################################################
+# load('data/cleandata_mat/matrix_1.RData')
+eval(parse(text=paste0("load('data/cleandata_mat/matrix_",args_sample,".RData')")))
+# n_matrix <- matrix_1
+eval(parse(text=paste0("n_matrix <- matrix_",args_sample)))
+# TimeFrame
+timeframe <- as.numeric(colnames(n_matrix))
+# NeuronActivity nactivity <- n_matrix[1,]
+eval(parse(text=paste0("nactivity <- n_matrix[",args_cell,",]")))
 ##################################################
 
-# correlogram
+
+# dataframe
 ##################################################
+data.frame(
+        TimeFrame = timeframe,
+        Nactivity = nactivity,
+        stringsAsFactors = FALSE
+) -> g
 ##################################################
 
-# dataframe for ggplot
+# acf
 ##################################################
+# acf(g$Nactivity, plot=T)
+g$Nactivity %>% acf(plot=T, lag.max=50)
+# library(magrittr)
+# g$Nactivity %>% acf(plot = T) %T>% print()
+##################################################
+# plot
+##################################################
+# png('output/WTS2/SampleNumber_1/CellNumber_1_CellType_X1.png', width = 100, height = 100)
+eval(parse(text=paste0("png('output/WTS2/SampleNumber_",args_sample,"/CellNumber_",args_cell,"_CellType_",args_celltype,".png', width = 500, height = 500)")))
+g$Nactivity %>% acf(plot=T, lag.max=50) %T>% print()
+dev.off() 
+##################################################
+# ggplot acf
+##################################################
+correlo <- acf(g$Nactivity, plot = FALSE)
+correlo_df <- with(correlo, data.frame(lag, acf))
+# gg <- ggplot(data=correlo_df, mapping=aes(x=lag, y=acf)) +
+#        geom_bar(stat = "identity", position = "identity")
+gg <- ggplot(data = correlo_df, mapping = aes(x = lag, y = acf)) +
+       geom_hline(aes(yintercept = 0)) +
+       geom_segment(mapping = aes(xend = lag, yend = 0))
+
+gg <- ggplot(data = correlo_df, mapping = aes(x = lag, y = acf)) +
+       geom_hline(aes(yintercept = 0)) +
+       geom_segment(mapping = aes(xend = lag, yend = 0))
 ##################################################
 
-# ggplot
+# ggacf
 ##################################################
+# ggacf
+g$Nactivity %>%
+    ggAcf(lag.max = 50, type = c("correlation"), plot = TRUE) -> p_acf
+# title name
+eval(parse(text=paste0("title <- ggtitle('SampleNumber",args_sample,"_CellNumber",args_cell,"_",args_celltype,"')")))
+# title theme
+t_1 <- theme(plot.title = element_text(size = 24, hjust = 0.5))
+
+gg <- p_acf +
+    title +
+    t_1
 ##################################################
