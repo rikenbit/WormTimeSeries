7abb813e249656d26cd3529d82c16514a0f7dcdd
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-02-18 11:27:31 +0900

    Pacs test

--- src/WTS2_correlogram.R
+++ src/WTS2_correlogram.R
@@ -1,13 +1,23 @@
 source("src/functions_WTS2.R")
 
-# args setting
-args <- commandArgs(trailingOnly = T)
+# # args setting
+# args <- commandArgs(trailingOnly = T)
+# # select animal number 個体番号の指定
+# args_sample <- args[1]
+# # select cell number 細胞番号の指定
+# args_cell <- args[2]
+# # select celltype 細胞型名の指定
+# args_celltype <- args[3]
+
+# test args
+##################################################
 # select animal number 個体番号の指定
-args_sample <- args[1]
+args_sample <- c("1")
 # select cell number 細胞番号の指定
-args_cell <- args[2]
+args_cell <- c("1")
 # select celltype 細胞型名の指定
-args_celltype <- args[3]
+args_celltype <-c("X1")
+##################################################
 
 # load
 ##################################################
@@ -30,20 +40,56 @@ data.frame(
 ) -> g
 ##################################################
 
-# ggacf
+# ggAcf
 ##################################################
 g$Nactivity %>%
-    ggAcf(lag.max = 50, type = c("correlation"), plot = TRUE) -> p_acf
+    ggAcf(lag.max = 50, type = c("correlation"), plot = TRUE) -> p_Acf
 # title name
-eval(parse(text=paste0("title <- ggtitle('SampleNumber",args_sample,"_CellNumber",args_cell,"_",args_celltype,"')")))
+eval(parse(text=paste0("title <- ggtitle('SampleNumber",args_sample,"_CellNumber",args_cell,"_",args_celltype,"_Acf')")))
 # title theme
 t_1 <- theme(plot.title = element_text(size = 24, hjust = 0.5))
 t_2 <- theme(axis.title = element_text(size = 16))
 
-gg <- p_acf +
+gg <- p_Acf +
     title +
     t_1+
     t_2
 
-eval(parse(text=paste0("ggsave(filename = 'output/WTS2/SampleNumber_",args_sample,"/CellNumber_",args_cell,"_CellType_",args_celltype,".png', plot = gg, dpi = 100, width = 7.0, height = 7.0)")))
+eval(parse(text=paste0("ggsave(filename = 'output/WTS2/SampleNumber_",args_sample,"/CellNumber_",args_cell,"_CellType_",args_celltype,"_Acf.png', plot = gg, dpi = 100, width = 7.0, height = 7.0)")))
+##################################################
+
+# ggPacf
+##################################################
+g$Nactivity %>%
+    ggPacf(lag.max = 50, type = c("correlation"), plot = TRUE) -> p_Pacf
+# title name
+eval(parse(text=paste0("title <- ggtitle('SampleNumber",args_sample,"_CellNumber",args_cell,"_",args_celltype,"_Pacf')")))
+# title theme
+t_1 <- theme(plot.title = element_text(size = 24, hjust = 0.5))
+t_2 <- theme(axis.title = element_text(size = 16))
+
+gg <- p_Pacf +
+    title +
+    t_1+
+    t_2
+
+eval(parse(text=paste0("ggsave(filename = 'output/WTS2/SampleNumber_",args_sample,"/CellNumber_",args_cell,"_CellType_",args_celltype,"_Pacf.png', plot = gg, dpi = 100, width = 7.0, height = 7.0)")))
+##################################################
+
+# ggAcf partial
+##################################################
+g$Nactivity %>%
+    ggAcf(lag.max = 50, type = c("partial"), plot = TRUE) -> p_pAcf
+# title name
+eval(parse(text=paste0("title <- ggtitle('SampleNumber",args_sample,"_CellNumber",args_cell,"_",args_celltype,"_pAcf')")))
+# title theme
+t_1 <- theme(plot.title = element_text(size = 24, hjust = 0.5))
+t_2 <- theme(axis.title = element_text(size = 16))
+
+gg <- p_pAcf +
+    title +
+    t_1+
+    t_2
+
+eval(parse(text=paste0("ggsave(filename = 'output/WTS2/SampleNumber_",args_sample,"/CellNumber_",args_cell,"_CellType_",args_celltype,"_pAcf.png', plot = gg, dpi = 100, width = 7.0, height = 7.0)")))
 ##################################################
