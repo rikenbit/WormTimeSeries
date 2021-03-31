b54e584d8a0417d47ddebca70fb0e6bcd0354a6e
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-03-31 20:47:51 +0900

    CCF_ACF matrix realdata 6000x10 stim all test

--- src/WTS2_heatmap.R
+++ src/WTS2_heatmap.R
@@ -1,24 +1,24 @@
 source("src/functions_WTS2.R")
 
 # #### args####
-# args <- commandArgs(trailingOnly = T)
-# # select data データの指定
-# args_data <- args[1]
-# # select timeframe 時系列区間の指定
-# args_TF <- args[2]
-# # select lag（ラグ） ラグの間隔の指定
-# args_lag <- as.numeric(args[3])
+args <- commandArgs(trailingOnly = T)
+# select data データの指定
+args_data <- args[1]
+# select timeframe 時系列区間の指定
+args_TF <- args[2]
+# select lag（ラグ） ラグの間隔の指定
+args_lag <- as.numeric(args[3])
 # # select Acf 自己相関の指定
 # args_Acf <- args[4]
-# # select animal number 個体番号の指定
-# args_sample <- args[5]
+# select animal number 個体番号の指定
+args_sample <- args[5]
 # # select cell number 細胞番号の指定
 # args_cell <- args[6]
 # # select celltype 細胞型名の指定
 # args_celltype <- args[7]
-# # outputファイル名
-# args_output <- args[8]
-# #######################
+# outputファイル名
+args_output <- args[8]
+#######################
 #######################
 #### test####
 # select data データの指定
@@ -26,18 +26,17 @@ args_data <- c("normalize_1")
 # select timeframe 時系列区間の指定
 args_TF <- c("all")
 # select lag（ラグ） ラグの間隔の指定
-# args_lag <- c("300")
 args_lag <- as.numeric(c("300"))
-# select Acf 自己相関の指定
-args_Acf <- c("Acf")
+# # select Acf 自己相関の指定
+# args_Acf <- c("Acf")
 # select animal number 個体番号の指定
 args_sample <- c("1")
-# select cell number 細胞番号の指定
-args_cell <- c("1")
-# select celltype 細胞型名の指定
-args_celltype <- c("102")
+# # select cell number 細胞番号の指定
+# args_cell <- c("1")
+# # select celltype 細胞型名の指定
+# args_celltype <- c("102")
 # outputファイル名
-args_output <- c("output/WTS2/correlogram/raw_CFP/all/50/Acf/SampleNumber_1/CellNumber_87_CellType_ADAR.png")
+args_output <- c("output/WTS2/heatmap/Data_normalize_1/TF_all/SampleNumber_1/τ1.png")
 ########################
 
 #### load NeuronActivity####
@@ -48,12 +47,37 @@ load(inputpath)
 eval(parse(text=paste0("ReadData <- ReadData_",args_sample)))
 mat <- as.matrix(ReadData)
 rownames(mat) <- rownames(ReadData)
-#### first stim TimeFrame####
 
-#### real data####
+#### load stimulation timing####
+# inputpath <- paste('data','stimulation', 'stim_1.RData', sep = '/')
+eval(parse(text=paste0("inputpath <- paste('data','stimulation', 'stim_",args_sample,".RData', sep = '/')")))
+load(inputpath)
+# stim <- stim_1
+eval(parse(text=paste0("stim <- stim_",args_sample)))
+stim %>% 
+    as.numeric() -> stimtiming
+timeframe <- as.numeric(rownames(ReadData))
+#dataframe
+data.frame(
+  TimeFrame = timeframe,
+  # Nactivity = nactivity,
+  StimTiming = stimtiming,
+  stringsAsFactors = FALSE
+) -> g_all
+
+#### first stim TimeFrame####
+length(stimtiming) -> stim_all
+g_all %>%
+    filter(StimTiming != 0) %>%
+        slice_head() %>%
+            .$TimeFrame -> stim_after
+stim_after -1 -> stim_before
+eval(parse(text=paste0("TF <- stim_",args_TF)))
+mat <- mat[1:TF,]
+#### CCF_ACF####
 #test
 args_lag <- as.numeric(c("1"))
-mat <-mat[1:6000,1:10]
+mat <- mat[,1:10]
 #
 CCF_ACF_mat <- sapply(1:ncol(mat), function(x) {
     sapply(1:ncol(mat), function(z){
@@ -87,19 +111,7 @@ CCF_ACF_df %>%
         pivot_longer(-cell_Sender, names_to = "cell_Receiver", values_to = "CCF_ACF") -> df
     
 #### ggplot####
-# ghm <- ggplot(df, aes(x = cell_Receiver, y = cell_Sender, fill = CCF_ACF))
-# ghm <- ghm + geom_tile()
+tilename <- paste0("TF",args_TF,"_SampleNumber",args_sample,"_τ",args_lag)
 
-ghm <- ggplot_ghm(df) + ggtitle(args_sample) +theme(plot.title = element_text(size = 30, hjust = 0.5))
-#### reorder#### 
-# CCF_ACF_mat %>%
-#     heatmap(., scale = "none", 
-#             hclustfun = function(x) {hclust(x, method = "ward.D2")})-> clr
-
-
-# cell_Receiver.idx <- colnames(CCF_ACF_mat)[clr$colInd]
-# df$cell_Receiver <- factor(df$cell_Receiver, levels = cell_Receiver.idx)
-# 
-# cell_Sender.idx <- colnames(CCF_ACF_mat)[clr$rowInd]
-# df$cell_Sender <- factor(df$cell_Sender, levels = cell_Sender.idx)
-#### ラグごとにアニメーションにするので並び替えしない####
+ghm <- ggplot_ghm(df) + ggtitle(tilename) +theme(plot.title = element_text(size = 30, hjust = 0.5))
+ggsave(filename = args_output, plot = ghm, dpi = 100, width = 10.0, height = 7.0)
--- src/functions_WTS2.R
+++ src/functions_WTS2.R
@@ -9,7 +9,6 @@ library(viridis)
 library(igraph)
 ##################################################
 ggplot_ghm = function(x) {
-# ggplot_ghm = function(x, N_ReadData=ReadData, GroupName=groupname) {
 	ghm <- ggplot(df, aes(x = cell_Receiver, y = cell_Sender, fill = CCF_ACF))
 	ghm <- ghm + geom_tile()
 	ghm <- ghm + theme_bw()
@@ -22,12 +21,5 @@ ggplot_ghm = function(x) {
 	                   strip.background = element_rect(fill = "white", colour = "white"),
 	                   axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 	ghm <- ghm + scale_fill_viridis(na.value = "white") #heatmap color is viridis
-	# # ghm <- ghm + xlab("timeframe") + ylab(GroupName)
-
-	# sX <- scale_x_continuous(name = "TimeFrame(1frame/0.2sec)",    # 軸の名前を変える
-	#                          breaks = seq(0, nrow(N_ReadData), by= 1000),     # 軸の区切りを0,2,4にする
-	# )
-	# ghm <- ghm +
-	#     sX
 	return(ghm)
 }
