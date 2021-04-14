125893cac97384d03de15bcfc8f5bab005b79b91
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-04-14 14:34:13 +0900

    WTS3 dist Rtsne add label test

--- src/WTS3_EUCL.R
+++ src/WTS3_EUCL.R
@@ -34,21 +34,30 @@ load(inputpath)
 # ReadData <- ReadData_1
 eval(parse(text=paste0("ReadData <- ReadData_",args_sample)))
 
-#### test####
-# ReadData <- ReadData[,1:50]
-########
-
 ### EUCL####
 # ユークリッド距離で距離行列を作成
-# #### test####
-# before <- Sys.time()
-# tSNE <- Rtsne(d, is_distance = TRUE, dims = 2, perplexity = 5, verbose = TRUE, max_iter = 1000)
-# after <- Sys.time()
-# Time <- after - before
-# save(tSNE, file="output/WTS3/EUCL/normalize_1/all/SampleNumber_1/tSNE.RData")
-# save(Time, file="output/WTS3/EUCL/normalize_1/all/SampleNumber_1/Time.RData")
-# ########
+### test####
+ReadData <- ReadData[1:5,1:5]
+########
 d <- diss(ReadData, "EUCL")
 tSNE <- Rtsne(d, is_distance = TRUE, dims = 2, perplexity = 5, verbose = TRUE, max_iter = 1000)
-########
 plot(tSNE$Y)
+
+### test####
+df_test <- data.frame(列A = c(0.2,0.3,0.4),
+                  列B = c(0.4,0.6,0.8),
+                  列C = c(0.19,0.29,0.39),
+                  列D = c(0.41,0.61,0.81),
+                  列E = c(0.39,0.59,0.79)
+                  )
+d_test <- diss(df_test, "EUCL")
+tSNE_test <- Rtsne(d_test, is_distance = TRUE, dims = 2, perplexity = 1, verbose = TRUE, max_iter = 1000)
+df_tSNE_test <- data.frame(tsne_1 = tSNE_test$Y[,1],
+                           tsne_2 = tSNE_test$Y[,2],
+                           列名 = attr(d_test, "Labels"))
+install.packages("ggrepel") 
+library("ggrepel")
+gg<- ggplot(df_tSNE_test, aes(x = tsne_1, y = tsne_2, label = 列名)) +
+    geom_point() +
+    geom_text_repel()
+########
