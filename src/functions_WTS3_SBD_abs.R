3c8b096a55ba59bcf92cf71bdb283f6f213ff335
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-08-16 14:46:37 +0900

    add tslist

--- src/functions_WTS3_SBD_abs.R
+++ src/functions_WTS3_SBD_abs.R
@@ -1010,6 +1010,64 @@ handle_rngkind <- function() {
 }
 ########
 
+# tslist(series, error.check) でエラー:関数 "tslist" を見つけることができませんでした
+# https://github.com/asardaes/dtwclust/blob/f4a5978ce051585d3c989e658c84d6b4b0fb77d4/R/UTILS-tslist.R
+########
+#' Coerce matrices or data frames to a list of time series
+#'
+#' Change a matrix or data frame to a list of univariate time series
+#'
+#' @export
+#'
+#' @param series A matrix or data frame where each row is a time series.
+#' @param simplify Coerce all series in the resulting list to either matrix (multivariate) or
+#'   numeric (univariate).
+#'
+#' @details
+#'
+#' Almost all functions in \pkg{dtwclust} work internally with lists of time series. If you want to
+#' avoid constant coercion, create a list of time series once by calling this function.
+#'
+#' For matrices and data frames, each **row** is considered as one time series. A list input is
+#' simply passed through.
+#'
+#' @return
+#'
+#' A list of time series.
+#'
+#' @note
+#'
+#' The function assumes that matrix-like objects can be first coerced via [base::as.matrix()], so
+#' that the result can be indexed with `series[i, ]`.
+#'
+#' No consistency checks are performed by this function.
+#'
+tslist <- function(series, simplify = FALSE) {
+    if (is.matrix(series) || is.data.frame(series)) {
+        rnms <- rownames(series)
+        mat <- unname(base::as.matrix(series))
+        series <- vector("list", nrow(mat))
+        if (!is.null(rnms)) setnames_inplace(series, rnms)
+        for (i in 1L:nrow(mat)) {
+            series[[i]] <- mat[i,]
+        }
+    }
+    else if (is.numeric(series))
+        series <- list(series) # nocov
+    else if (!is.list(series))
+        stop("Unsupported data type.")
+    # coerce to simple types that are known to work
+    if (simplify) {
+        if (is_multivariate(series))
+            series <- lapply(series, base::as.matrix)
+        else
+            series <- lapply(series, base::as.numeric)
+    }
+    # return
+    series
+}
+########
+
 .sbd_y = function(x) {
     shift_2 <- ReadData.list[[x]] %>% as.numeric()
     sbd <- dtwclust::SBD(shift_1,
