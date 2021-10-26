55209d940898f0955e6a087a3a92a3bbff7b723f
yamaken37 <58546728+yamaken37@users.noreply.github.com>
2021-10-26 12:35:51 +0900

    WTS4_Membership snakemake

--- src/WTS4_Membership.R
+++ src/WTS4_Membership.R
@@ -1,6 +1,7 @@
-source("src/functions_WTS4_Membership.R")
+# libraryインストールがないので
+# source("src/functions_WTS4_Membership.R")
 
-#### args setting####
+### args setting####
 args <- commandArgs(trailingOnly = T)
 # No. of Clusters 
 args_k <- args[1]
@@ -11,9 +12,9 @@ args_output_membership <- args[3]
 
 #### test args####
 # No. of Clusters 
-args_k <- c("3")
-args_input_dist <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/SampleNumber_1/SBD_abs.RData")
-args_output_membership  <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/SampleNumber_1/3_Clusters/Membership.RData")
+# args_k <- c("3")
+# args_input_dist <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/SampleNumber_1/SBD_abs.RData")
+# args_output_membership  <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/SampleNumber_1/3_Clusters/Membership.RData")
 
 
 #### No. of Clusters####
@@ -30,24 +31,25 @@ k <- args_k
 
 # load Distance matrix
 load(args_input_dist)
+# dist()で変換しないとエラーになる場合がある
 D1 <- d
 
 # D <- list(D1, D2, D3)
 D <- list(D1)
 
 # Clustering against each distance matrix
-C <- lapply(D, function(d, k){
-	cutree(hclust(d, method="ward.D2"), k)
-}, k=k)
+C <- lapply(D, function(d, k) {
+    cutree(hclust(d, method="ward.D2"), k)
+    }, k=k)
 
 # Cluster Labels → Indicator Matrices
-Hs <- lapply(C, function(x){
-	out <- matrix(0, nrow=length(x), ncol=length(unique(x)))
-	for(i in seq_along(x)){
-		out[i,x[i]] <- 1
-	}
-	out
-})
+Hs <- lapply(C, function(x) {
+    out <- matrix(0, nrow=length(x), ncol=length(unique(x)))
+  	for(i in seq_along(x)) {
+  		  out[i,x[i]] <- 1
+	  }
+	  out
+    })
 
 #### ggsave####
 save(Hs, file=args_output_membership)
