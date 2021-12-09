053d2756d23b0eecdc6c316a3791a1a19024d146
yamaken37 <samadakenji@gmail.com>
2021-12-09 15:34:11 +0900

    WTS4 EvalPlot for behavior

--- src/WTS4_EvalPlot.R
+++ src/WTS4_EvalPlot.R
@@ -1,6 +1,6 @@
 source("src/functions_WTS4_EvalPlot.R")
 
-#### args setting####
+# #### args setting####
 args <- commandArgs(trailingOnly = T)
 # input directory path
 args_input_path <- args[1]
@@ -13,7 +13,8 @@ args_method <- args[3]
 # #### test args####
 # args_input_path <- c("output/WTS4/normalize_1/stimAfter/SBD_abs")
 # # output
-# args_output <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/CSPA/EvalPlot.png")
+# # args_output <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/CSPA/EvalPlot_docker.png")
+# args_output <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/CSPA/EvalPlot_behavior.png")
 # # ReClustering Method
 # args_method <- c("CSPA")
 
@@ -26,14 +27,14 @@ list.files(args_input_path,
 
 grep(args_method, list_eval_data) %>% 
     list_eval_data[.] -> list_cls_method
-########################################
-# docker 再クラスラスタリングで再実行対応
-grep("_docker", list_cls_method) %>% 
-    list_cls_method[.] -> list_cls_method
-########################################
+
+# #### docker 再クラスラスタリングで再実行対応####
+# grep("_docker", list_cls_method) %>%
+#     list_cls_method[.] -> list_cls_method
 
 #### load  value PseudoF####
-grep("PseudoF", list_cls_method) %>% 
+# grep("PseudoF", list_cls_method) %>% 
+grep("PseudoF/eval_result_docker", list_cls_method) %>% # behavior
       list_cls_method[.] -> input_path_PseudoF
 
 PseudoF_value <- numeric()
@@ -43,7 +44,8 @@ for(i in 1:length(input_path_PseudoF)){
     PseudoF_value <- c(PseudoF_value, eval_result)
 }
 #### load  value Connectivity####
-grep("Connectivity", list_cls_method) %>% 
+# grep("Connectivity", list_cls_method) %>% 
+grep("Connectivity/eval_result_docker", list_cls_method) %>% # behavior
     list_cls_method[.] -> input_path_Connectivity
 
 Connectivity_value <- numeric()
@@ -54,7 +56,8 @@ for(i in 1:length(input_path_Connectivity)){
 }
 
 #### load  value ARI####
-grep("ARI", list_cls_method) %>% 
+# grep("ARI", list_cls_method) %>%
+grep("ARI_behavior", list_cls_method) %>%  # behavior
   list_cls_method[.] -> input_path_ARI
 
 ARI_value <- numeric()
@@ -64,7 +67,8 @@ for(i in 1:length(input_path_ARI)){
   ARI_value <- c(ARI_value, eval_result)
 }
 #### load  value purity####
-grep("purity", list_cls_method) %>% 
+# grep("purity", list_cls_method) %>%
+grep("purity_behavior", list_cls_method) %>%  # behavior
   list_cls_method[.] -> input_path_purity
 
 purity_value <- numeric()
@@ -74,8 +78,9 @@ for(i in 1:length(input_path_purity)){
   purity_value <- c(purity_value, eval_result)
 }
 #### load  value Fmeasure####
-grep("Fmeasure", list_cls_method) %>% 
-  list_cls_method[.] -> input_path_Fmeasure
+# grep("Fmeasure", list_cls_method) %>% 
+grep("Fmeasure_behavior", list_cls_method) %>%   # behavior
+    list_cls_method[.] -> input_path_Fmeasure
 
 Fmeasure_value <- numeric()
 
@@ -84,7 +89,8 @@ for(i in 1:length(input_path_Fmeasure)){
   Fmeasure_value <- c(Fmeasure_value, eval_result)
 }
 #### load  value Entropy####
-grep("Entropy", list_cls_method) %>% 
+# grep("Entropy", list_cls_method) %>% 
+grep("Entropy_behavior", list_cls_method) %>%   # behavior
   list_cls_method[.] -> input_path_Entropy
 
 Entropy_value <- numeric()
@@ -120,7 +126,7 @@ df_eval_long <- df_eval %>%
 str_remove(args_output, 
            "output/WTS4/normalize_1/stimAfter/") %>% 
     str_remove(., 
-               "/EvalPlot.png") -> plot_title
+               ".png") -> plot_title
 # 参考 https://stats.biopapyrus.jp/r/ggplot/geom_bar.html
 #### ggplot PseudoF Connectivity####
 df_eval_long %>% 
@@ -133,17 +139,50 @@ df_eval_long %>%
         geom_line(size = 3) +
         theme(text = element_text(size = 30)) +
         scale_x_continuous(breaks=seq(2,20,1)) -> gg_no_label
-#### ggplot label####
+# #### ggplot label####
+# df_eval_long %>% 
+#     dplyr::filter(., Eval=="ARI" | Eval=="purity" | Eval=="Fmeasure") %>%
+#         ggplot(., 
+#                aes(x = Cluster, 
+#                    y = Eval_Value, 
+#                    colour = Eval)
+#                ) +
+#         geom_line(size = 3) +
+#         theme(text = element_text(size = 30)) +
+#         scale_x_continuous(breaks=seq(2,20,1)) -> gg_label
+#### ggplot ARI####
 df_eval_long %>% 
-    dplyr::filter(., Eval=="ARI" | Eval=="purity" | Eval=="Fmeasure") %>%
+    dplyr::filter(., Eval=="ARI") %>%
         ggplot(., 
                aes(x = Cluster, 
                    y = Eval_Value, 
                    colour = Eval)
-               ) +
+        ) +
         geom_line(size = 3) +
         theme(text = element_text(size = 30)) +
-        scale_x_continuous(breaks=seq(2,20,1)) -> gg_label
+        scale_x_continuous(breaks=seq(2,20,1)) -> gg_label_ARI
+#### ggplot purity####
+df_eval_long %>% 
+    dplyr::filter(., Eval=="purity") %>%
+        ggplot(., 
+               aes(x = Cluster, 
+                   y = Eval_Value, 
+                   colour = Eval)
+        ) +
+        geom_line(size = 3) +
+        theme(text = element_text(size = 30)) +
+        scale_x_continuous(breaks=seq(2,20,1)) -> gg_label_purity
+#### ggplot Fmeasure####
+df_eval_long %>% 
+    dplyr::filter(., Eval=="Fmeasure") %>%
+        ggplot(., 
+               aes(x = Cluster, 
+                   y = Eval_Value, 
+                   colour = Eval)
+        ) +
+        geom_line(size = 3) +
+        theme(text = element_text(size = 30)) +
+        scale_x_continuous(breaks=seq(2,20,1)) -> gg_label_Fmeasure
 #### ggplot Entropy####
 df_eval_long %>% 
     dplyr::filter(., Eval=="Entropy") %>%
@@ -165,17 +204,27 @@ c("PseudoF","ARI","purity","Fmeasure") %>%
     purrr::map_int(., eval_max) -> eval_id_max
 sort(c(eval_id_min, eval_id_max)) %>% 
     sort() -> eval_id
-# BESTなクラスタ数の情報を表示
+BESTなクラスタ数の情報を表示
 df_eval_long_ID[eval_id,] %>% 
     dplyr::select(Eval,Cluster,Eval_Value) %>% 
         mutate_if(is.numeric, round, digits = 3) %>% 
             ggtexttable(rows = NULL, theme = ttheme(base_size = 60)) -> gg_eval_table
-
+eval_arrange <- c("Connectivity", "PseudoF", "ARI", "purity", "Fmeasure", "Entropy")
+df_eval_long_ID[eval_id,] %>% 
+    dplyr::select(Eval,Cluster,Eval_Value) %>%
+        mutate(Eval = factor(Eval, levels = eval_arrange)) %>% 
+            arrange(Eval) %>% 
+                mutate(Eval = as.character(Eval)) %>% 
+                    mutate_if(is.numeric, round, digits = 3) %>% 
+                        ggtexttable(rows = NULL, theme = ttheme(base_size = 60)) -> gg_eval_table
 #### patchwork####
 gg <- gg_no_label +
-    gg_label +
+    # gg_label +
+    gg_label_ARI +
+    gg_label_purity +
+    gg_label_Fmeasure +
     gg_label_Entropy +
-  gg_eval_table +
+    gg_eval_table +
     plot_layout(ncol = 1) +
     plot_annotation(title = plot_title,
                     caption = 'made with patchwork',
@@ -187,6 +236,7 @@ ggsave(filename = args_output,
        plot = gg,
        dpi = 100, 
        width = 30.0, 
-       height = 30.0,
+       # height = 30.0,
+       height = 60.0,
        limitsize = FALSE
        )
--- workflow/WTS4_EvalPlot.smk
+++ workflow/WTS4_EvalPlot.smk
@@ -12,27 +12,42 @@ ReClustering_method = ["CSPA","OINDSCAL","MCMIHOOI"]
 
 rule all:
     input:
-        expand('output/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.png',
+        expand('output/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_behavior.png',
             range=time_range,
             dist=dist_data,
             Re_cls=ReClustering_method
             )
-        
-rule EvalPlot_docker:
+rule EvalPlot_behavior:
     output:
-        EvalPlot = 'output/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.png'
+        EvalPlot = 'output/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_behavior.png'
     params:
         input_path = 'output/WTS4/normalize_1/{range}/{dist}'
     benchmark:
-        'benchmarks/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.txt'
+        'benchmarks/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_behavior.txt'
     conda:
         '../envs/myenv_WTS4_EvalPlot.yaml'
     resources:
         mem_gb=200
     log:
-        'logs/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.log'
+        'logs/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_behavior.log'
     shell:
         'src/WTS4_EvalPlot.sh {params.input_path} {output.EvalPlot} {wildcards.Re_cls} >& {log}'
+
+# rule EvalPlot_docker:
+#     output:
+#         EvalPlot = 'output/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.png'
+#     params:
+#         input_path = 'output/WTS4/normalize_1/{range}/{dist}'
+#     benchmark:
+#         'benchmarks/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.txt'
+#     conda:
+#         '../envs/myenv_WTS4_EvalPlot.yaml'
+#     resources:
+#         mem_gb=200
+#     log:
+#         'logs/WTS4/normalize_1/{range}/{dist}/{Re_cls}/EvalPlot_docker.log'
+#     shell:
+#         'src/WTS4_EvalPlot.sh {params.input_path} {output.EvalPlot} {wildcards.Re_cls} >& {log}'
 
 # rule EvalPlot:
 #     output:
