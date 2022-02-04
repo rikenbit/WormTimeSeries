fcec8752f26a040b887443c12a75926d09873bf5
yamaken <samadakenji@gmail.com>
2022-02-04 17:30:02 +0900

    WTS4_Behavior_ClsCount fix normalize & add others & fix NaN

--- src/functions_WTS4_Behavior_ClsCount.R
+++ src/functions_WTS4_Behavior_ClsCount.R
@@ -17,9 +17,18 @@ library(openxlsx)
   out
 }
 
+# 自分以外のメンバー数 正規化用
+.other_member <- function(H_label){
+  out <- rep(0, length=nrow(H_label))
+  for(i in seq_len(nrow(H_label))){
+    idx1 <- which(H_label[i, ] == 1)
+    out[i] <- sum(H_label[,idx1]) - 1
+  }
+  out
+}
+
 .df_count = function(x) {
   #### 各個体の行動ラベル回数データフレーム作成####
-  # x <- 1
   cls_n <- C[[x]]
   
   df_cls <- as.data.frame(cls_n)
@@ -31,13 +40,17 @@ library(openxlsx)
                         df_cls, 
                         by.x = "CellType", 
                         by.y = "cell_type", 
-                        all.x = TRUE)
-  df_cls_label_rmNA <- na.omit(df_cls_label)
+                        all.y = TRUE)
+  # NA to others
+  df_cls_label$Classes <- ifelse(is.na(df_cls_label$Classes), "others", df_cls_label$Classes)
   
-  label <- df_cls_label_rmNA$Classes
-  cluster <- df_cls_label_rmNA$Cluster
+  label <- df_cls_label$Classes
+  cluster <- df_cls_label$Cluster
   H_label <- .H(label)
   H_cluster <- .H(cluster)
+  
+  other_member <- .other_member(H_label)
+  
   # カウント
   count1 <- rep(0, length=length(label))
   for(i in seq_len(nrow(H_label))){
@@ -45,9 +58,14 @@ library(openxlsx)
     idx2 <- which(H_cluster[i, ] == 1)
     count1[i] <- sum(H_label[,idx1] * H_cluster[,idx2]) - 1
   }
-  # 1個体分 count1
-  df_cls_label_rmNA %>% 
+  # 正規化
+  count1 <- count1 / other_member
+  # 0/0のNaN対策
+  count1 <- ifelse(is.nan(count1), 0, count1)
+  # 1個体分 count1 
+  df_cls_label %>% 
     mutate(Count=count1) -> df_count
+  df_count <- df_count[grep("^[0-9]",df_count$CellType, invert=TRUE),]
 	return(df_count)
 }
 
