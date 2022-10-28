2343a6b031ce07dd8892927a86c9558dad924201
yamaken37 <samadakenji@gmail.com>
2022-10-28 18:04:31 +0900

    WTS4_yshift_df.R：transform to dataframe

--- src/WTS4_yshift_df.R
+++ src/WTS4_yshift_df.R
@@ -6,13 +6,20 @@ args_input_yshift <- args[1]
 #### test args####
 # INPUT yshift
 args_input_yshift <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Shift_F/SampleNumber_1.RData")
-
+# INPUT mSBD
+args_input_mSBD <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Distance/SampleNumber_1.RData")
 #### load####
 load(args_input_yshift)
-#### ?####
+#### df yshift####
 indices <- t(combn(seq(nrow(shift_matrix)), 2))
 purrr::map_dfr(1:nrow(indices), .yshift_dfr) -> yshift_df
+yshift_df[order(yshift_df$cell_cell),] -> yshift_df_sort
 
-#### ?####
-#### ?####
+#### df mSBD####
+load(args_input_mSBD)
+d_f <- .filter_cellnames(d)
+mat_d_f <- as.matrix(d_f)
+purrr::map_dfr(1:nrow(indices), .mSBD_dfr) -> mSBD_df
+
+#### merge df####
 #### save####
--- src/functions_WTS4_yshift_df.R
+++ src/functions_WTS4_yshift_df.R
@@ -1,6 +1,7 @@
 #library
 ##################################################
 library(tidyverse)
+library(usedist)
 ##################################################
 .func = function(x) {
 	return(return_object)
@@ -16,4 +17,23 @@ library(tidyverse)
                stringsAsFactors = FALSE
     ) -> return_object
     return(return_object)
+}
+.mSBD_dfr = function(x) {
+    cell_2 <-colnames(mat_d_f)[indices[x,][1]]
+    cell_1 <- rownames(mat_d_f)[indices[x,][2]]
+    mat_d_f[indices[x,][2],indices[x,][1]]
+    
+    data.frame(cell_cell = paste0(cell_1, "_", cell_2),
+               mSBD = mat_d_f[indices[x,][2],indices[x,][1]],
+               stringsAsFactors = FALSE
+    ) -> return_object
+    return(return_object)
+}
+
+.filter_cellnames <- function(X){
+    D <- X
+    D_cell <- attr(D, "Labels")
+    D_cell_f <- D_cell[grep("^[0-9]", D_cell, invert=TRUE)]
+    D_f <- dist_subset(D, D_cell_f)
+    D_f
 }
