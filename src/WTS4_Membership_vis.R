b428b5cca57bbd5d98218f160e7f657a488f81f5
yamaken37 <samadakenji@gmail.com>
2022-11-08 11:08:01 +0900

    WTS4_Membership_vis.smk

--- src/WTS4_Membership_vis.R
+++ src/WTS4_Membership_vis.R
@@ -1,18 +1,25 @@
 source("src/functions_WTS4_Membership_vis.R")
 
 #### args setting####
+args <- commandArgs(trailingOnly = T)
+# input
+args_input <- args[1]
+# output
+args_output <- args[2]
 #### test args####
-args_input <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Membership_df/k_Number_6/SampleNumber_1.RData")
-args_output <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Membership_vis/k_Number_6/SampleNumber_1.png")
+# args_input <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Membership_df/k_Number_6/SampleNumber_1.RData")
+# args_output <- c("output/WTS4/normalize_1/stimAfter/SBD_abs/Membership_vis/k_Number_6/SampleNumber_1.png")
+
 #### load yshift_mem_df####
 load(args_input)
+
 #### filter col cell_cell,mem,shift####
 yshift_mem_df %>% 
     dplyr::select(cell_cell, member, yshift) -> ggdata
+
 #### ggplot####
 gg_box <- ggplot(ggdata, aes(x=factor(member), y=yshift)) +
     guides(colour="none") +
-    # geom_boxplot(alpha =0.8) +
     #外れ値のプロットを省く https://stats.biopapyrus.jp/r/ggplot/geom-boxplot.html
     geom_boxplot(outlier.shape = NA, alpha =0.8) +
     # geom_jitterを使うとかぶらない http://sakananoiroiro.seesaa.net/article/454739891.html
@@ -26,13 +33,7 @@ gg_box <- ggplot(ggdata, aes(x=factor(member), y=yshift)) +
 
 gg_vio <- ggplot(ggdata, aes(x=factor(member), y=yshift)) +
     guides(colour="none") +
-    # geom_boxplot(alpha =0.8) +
-    #外れ値のプロットを省く https://stats.biopapyrus.jp/r/ggplot/geom-boxplot.html
-    # geom_boxplot(outlier.shape = NA, alpha =0.8) +
     geom_violin() +
-    # geom_jitterを使うとかぶらない http://sakananoiroiro.seesaa.net/article/454739891.html
-    # geom_point(position = position_jitter(width=0.05), size = 3.0 ,alpha = 0.7) +
-    # geom_jitter(size = 0.8,alpha = 0.7) +
     xlab("Membership") +
     ylab("yshift") +
     scale_y_continuous(limits = c(-6000, 6000)) +
@@ -70,8 +71,7 @@ data.frame(T_pvalue = T_pvalue,
            row.names = NULL) -> t_table
 t_table %>% 
     ggtexttable(rows = NULL, 
-                theme = ttheme(base_size = 60, 
-                               base_style ="mBlue")
+                theme = ttheme(base_size = 60, base_style ="mBlue")
                 ) -> gg_table
 #### graph title####
 args_output |> 
@@ -101,4 +101,4 @@ ggsave(filename = args_output,
        width = 20.0, 
        height = 20.0,
        limitsize = FALSE
-)
+       )
