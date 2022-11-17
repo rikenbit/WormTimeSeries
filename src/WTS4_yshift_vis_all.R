8c015cdbf28fb69e15832b8f160d045e2c3cc58c
yamaken37 <samadakenji@gmail.com>
2022-11-17 17:41:35 +0900

    fix ggplot yshift_vis_all

--- src/WTS4_yshift_vis_all.R
+++ src/WTS4_yshift_vis_all.R
@@ -37,23 +37,23 @@ dfr_yshift <- purrr::map_dfr(names(DFs), .DFs_yshift)
 #     theme(plot.title = element_text(hjust = 0.5)) +
 #     theme(text = element_text(size = 90))
 
-#### ggplot density group####
-cord_x <- c("yshift")
-cord_y <- c("density")
-ggplot_title <- c("Animal_all")
-gg <- ggplot(dfr_yshift,
-             aes(x = yshift,
-                 fill=factor(animal, levels = names(DFs)))
-             ) +
-    geom_density(alpha=0.2) +
-    labs(x = cord_x,
-         y = cord_y,
-         title = ggplot_title,
-         fill = "Animal_No.") +
-    ylim(c(0, 0.05)) +
-    xlim(c(-1000, 1000)) +
-    theme(plot.title = element_text(hjust = 0.5)) +
-    theme(text = element_text(size = 90))
+# #### ggplot density group####
+# cord_x <- c("yshift")
+# cord_y <- c("density")
+# ggplot_title <- c("Animal_all")
+# gg <- ggplot(dfr_yshift,
+#              aes(x = yshift,
+#                  fill=factor(animal, levels = names(DFs)))
+#              ) +
+#     geom_density(alpha=0.2) +
+#     labs(x = cord_x,
+#          y = cord_y,
+#          title = ggplot_title,
+#          fill = "Animal_No.") +
+#     ylim(c(0, 0.05)) +
+#     xlim(c(-1000, 1000)) +
+#     theme(plot.title = element_text(hjust = 0.5)) +
+#     theme(text = element_text(size = 90))
 # #### ggplot density####
 # cord_x <- c("yshift")
 # cord_y <- c("density")
@@ -70,6 +70,74 @@ gg <- ggplot(dfr_yshift,
 #     xlim(c(-1000, 1000)) +
 #     theme(plot.title = element_text(hjust = 0.5)) +
 #     theme(text = element_text(size = 90))
+
+#### ggplot density group####
+cord_x <- c("1-mSBD")
+cord_y <- c("density")
+ggplot_title <- c("Animal_all")
+
+# # case_when https://www.jaysong.net/RBook/visualization4.html
+# dfr_yshift %>% mutate(yshift_abs = case_when(abs(yshift) < 100 ~ "100",
+#                                              abs(yshift) < 200 ~ "101~200",
+#                                              abs(yshift) < 300 ~ "201~300",
+#                                              TRUE              ~ "300以上"),
+#                       yshift_abs = factor(yshift_abs, 
+#                                           levels = c("0~100",
+#                                                      "101~200",
+#                                                      "201~300",
+#                                                      "300以上"))
+#                       ) -> dfr_yshift_group
+
+# case_when https://www.jaysong.net/RBook/visualization4.html
+dfr_yshift %>% mutate(yshift_abs = case_when(abs(yshift) <= 100 ~ "0~100",
+                                             abs(yshift) <= 200 ~ "101~200",
+                                             abs(yshift) <= 300 ~ "201~300",
+                                             abs(yshift) <= 400 ~ "301~400",
+                                             abs(yshift) <= 500 ~ "401~500",
+                                             abs(yshift) <= 600 ~ "501~600",
+                                             abs(yshift) <= 700 ~ "601~700",
+                                             abs(yshift) <= 800 ~ "701~800",
+                                             abs(yshift) <= 900 ~ "801~900",
+                                             abs(yshift) <= 1000 ~ "901~1000",
+                                             TRUE              ~ "1001~"),
+                      # yshift_abs = factor(yshift_abs,
+                      #                     levels = c("0~100",
+                      #                                "101~200",
+                      #                                "201~300",
+                      #                                "301~400",
+                      #                                "401~500",
+                      #                                "501~600",
+                      #                                "601~700",
+                      #                                "701~800",
+                      #                                "801~900",
+                      #                                "901~1000",
+                      #                                "1001以上"))
+                      yshift_abs = factor(yshift_abs,
+                                          levels = c("1001~",
+                                                     "901~1000",
+                                                     "801~900",
+                                                     "701~800",
+                                                     "601~700",
+                                                     "501~600",
+                                                     "401~500",
+                                                     "301~400",
+                                                     "201~300",
+                                                     "101~200",
+                                                     "0~100"))
+                      ) -> dfr_yshift_group
+
+gg <- ggplot(dfr_yshift_group,
+             # dfr_yshift_group[dfr_yshift_group$yshift_abs!="1000以上",],
+             aes(x = 1-mSBD,
+                 fill=yshift_abs)
+             ) +
+    geom_density(alpha=0.8) +
+    labs(x = cord_x,
+         y = cord_y,
+         title = ggplot_title,
+         fill = "yshift_abs") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(text = element_text(size = 90))
 
 # #### ggplot density 2d####
 # # https://mukkujohn.hatenablog.com/entry/2016/09/25/164940
